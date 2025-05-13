262a3b5ed32845e1525132518270e4a94fb336c2
default <annasophia.sanchez-acosta@stu.ocsb.ca>
2025-05-13 16:45:48 +0000

    number

--- js/gameScene.js
+++ js/gameScene.js
@@ -53,8 +53,8 @@ class GameScene extends Phaser.Scene {
   /**
    * Should be overridden by your own Scenes.
    * This method is called once per game step while the scene is running.
-   * @param {} time - The current time
-   * @param {} delta The delta time in ms since the last frame.
+   * @param {number} time - The current time
+   * @param {number} delta The delta time in ms since the last frame.
    */
   update(time, delta) {
     // pass
