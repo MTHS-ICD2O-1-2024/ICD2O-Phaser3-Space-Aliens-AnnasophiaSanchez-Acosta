d236bf2c5a36136320932d5c82f7c55c7bf2b42c
default <annasophia.sanchez-acosta@stu.ocsb.ca>
2025-05-21 17:26:01 +0000

    why

--- js/gameScene.js
+++ js/gameScene.js
@@ -55,6 +55,7 @@ class GameScene extends Phaser.Scene {
 
     // sound
     this.load.audio("laser", "./assets/laser1.wav")
+    this.load.audio("explosion", "./assets/barrelExplosion.wav")
   }
 
   /**Can be defined on your own Scenes.
@@ -73,6 +74,16 @@ class GameScene extends Phaser.Scene {
     // create a group for aliens
     this.alienGroup = this.add.group()
     this.createAlien()
+
+    // Collisions between missiles and aliens
+    this.physics.add.collider(this.missileGroup, this.alienGroup, function (missileCollide, alienCollide) {
+      alienCollide.destroy()
+      missileCollide.destroy()
+      this.sound.play("explosion")
+      this.createAlien()
+      this.createAlien()
+      }.bind(this))
+    }
   }
 
   /**
