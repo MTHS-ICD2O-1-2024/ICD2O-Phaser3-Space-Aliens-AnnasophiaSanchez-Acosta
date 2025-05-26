fac4aeaccc7a90cbdb3cbacac8e3703b0da383aa
AnnasophiaSanchez-Acosta <annasophia.sanchez-acosta@stu.ocsb.ca>
2025-05-26 01:26:19 +0000

    game over

--- js/gameScene.js
+++ js/gameScene.js
@@ -31,6 +31,7 @@ class GameScene extends Phaser.Scene {
     this.score = 0
     this.scoreText = null
     this.scoreTextStyle = { font: "65px Arial", fill: "#ffffff", align: "center" }
+    this.gameOverTextStyle = { font: "65px Arial", fill: "#ff0000", align: "center" }
   }
 
   /**
@@ -59,6 +60,7 @@ class GameScene extends Phaser.Scene {
     // sound
     this.load.audio("laser", "assets/laser1.wav")
     this.load.audio("explosion", "assets/barrelExploding.wav")
+    this.load.audio("bomb", "assets/bomb.wav")
   }
 
   /**Can be defined on your own Scenes.
@@ -94,6 +96,16 @@ class GameScene extends Phaser.Scene {
         this.createAlien()
       }.bind(this)
     )
+    // Collisions between ship and aliens
+    this.physics.add.collider(this.ship, this.alienGroup, function (shipCollide, alienCollide){
+      this.sound.play("bomb")
+      this.physics.pause()
+      alienCollide.destroy
+      shipCollide.destroy
+      this.gameOverText = this.add.text(1920 / 2, 1080 / 2, "Game Over!\nClick to play again.", this.gameOverTextStyle).setOrigin(0.5)
+      this.gameOverText.setInteractive ({ useHandCursor: true})
+      this.gameOverText.on("pointerdown",() => this.scene.start("menuScene"))
+    }.bind(this))
   }
 
   update(time, delta) {
