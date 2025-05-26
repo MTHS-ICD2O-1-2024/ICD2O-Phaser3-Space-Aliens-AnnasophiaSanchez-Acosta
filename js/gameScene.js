dc6cc784021bbb0a45c433b59333e7145e5cd4dd
AnnasophiaSanchez-Acosta <annasophia.sanchez-acosta@stu.ocsb.ca>
2025-05-26 02:03:24 +0000

    o

--- js/gameScene.js
+++ js/gameScene.js
@@ -100,8 +100,8 @@ class GameScene extends Phaser.Scene {
     this.physics.add.collider(this.ship, this.alienGroup, function (shipCollide, alienCollide){
       this.sound.play("bomb")
       this.physics.pause()
-      alienCollide.destroy
-      shipCollide.destroy
+      alienCollide.destroy()
+      shipCollide.destroy()
       this.gameOverText = this.add.text(1920 / 2, 1080 / 2, "Game Over!\nClick to play again.", this.gameOverTextStyle).setOrigin(0.5)
       this.gameOverText.setInteractive ({ useHandCursor: true})
       this.gameOverText.on("pointerdown",() => this.scene.start("menuScene"))
